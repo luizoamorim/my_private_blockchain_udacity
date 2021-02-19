f515e314d634be5c9c6952af696be3c0dc26c8d0
Luiz <luiz.oamorim@gmail.com>
2021-02-18 23:31:42 -0300

    fix(blockchain.js - requisite 1): Correct presentation of request message

--- src/blockchain.js
+++ src/blockchain.js
@@ -97,9 +97,7 @@ class Blockchain {
      */
     requestMessageOwnershipVerification(address) {
         return new Promise((resolve) => {
-            resolve(`
-                ${address}:${new Date().getTime().toString().slice(0,-3)}:starRegistry
-            `)
+            resolve(`${address}:${new Date().getTime().toString().slice(0,-3)}:starRegistry`)
         });
     }
 
